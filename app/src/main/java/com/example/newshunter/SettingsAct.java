721327e6a8fc7472e55487af60ff0ec2722d0119
Kushal-Gera <kushalgera1212@gmail.com>
2019-09-24 15:42:23 +0530

    rate and share = backend

--- app/src/main/java/com/example/newshunter/SettingsAct.java
+++ app/src/main/java/com/example/newshunter/SettingsAct.java
@@ -2,6 +2,7 @@ package com.example.newshunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,8 +18,10 @@ import com.r0adkll.slidr.Slidr;
 public class SettingsAct extends AppCompatActivity {
     private static final String TAG = "SettingsAct";
     public static final String GMAIL_LINK= "kushalgera1212@gmail.com";
+    public static final String APP_LINK= "http://play.google.com/store/apps/details?id=" + "com.android.chrome";
+    //                            KUri.parse("http://play.google.com/store/apps/details?id=" + getPackageName() )));   to be used
 
-    LinearLayout developer, suggest;
+    LinearLayout developer, suggest, share, rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,8 @@ public class SettingsAct extends AppCompatActivity {
 
         developer = findViewById(R.id.developer);
         suggest = findViewById(R.id.suggest);
+        share = findViewById(R.id.Share_app);
+        rate = findViewById(R.id.rate);
 
         developer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +52,37 @@ public class SettingsAct extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, APP_LINK);
+
+                startActivity(Intent.createChooser(i, "Share via"));
+            }
+        });
+
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open my app in playStore
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+//                            Uri.parse("market://details?id=" + getPackageName() )));    to be used...
+                            Uri.parse("market://details?id=" + "com.android.chrome" )));
+
+                }
+                catch (ActivityNotFoundException e){
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+//                            Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName() )));   to be used
+                            Uri.parse(APP_LINK)));
+                }
+
+            }
+        });
+
 
 
     }
