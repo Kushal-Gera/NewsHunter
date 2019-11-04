79ddbbf86619cbce221bc7e4c71f5f0ec7d03b01
Kushal-Gera <kushalgera1212@gmail.com>
2019-11-04 19:03:23 +0530

    changes

--- app/src/main/java/kushal/application/newshunter/MyMessagingService.java
+++ app/src/main/java/kushal/application/newshunter/MyMessagingService.java
@@ -2,6 +2,8 @@ package kushal.application.newshunter;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -21,15 +23,23 @@ public class MyMessagingService extends FirebaseMessagingService {
         showNoti(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
     }
 
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+
+    }
+
     public void showNoti(String title, String text){
 
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION)
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.logo)
                 .setAutoCancel(true)
+                .setLargeIcon(bitmap)
                 .setContentIntent(pendingIntent)
                 .setContentText(text);
 
